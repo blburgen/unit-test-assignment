76a7d6e1f37b9d77f4c910931e12a764228624ab
Brady Burgener <bradylbsee@gmail.com>
2025-01-29 12:36:06 -0700

    Week 12

---
+++ src/main/java/com/promineotech/TestDemo.java
@@ -0,0 +1,41 @@
+package com.promineotech;
+
+public class TestDemo {
+
+	/*
+	 * This method adds two positive number together. If any of the numbers is not
+	 * positive it prints out an exception.
+	 */
+	public int addPositive(int a, int b) {
+		if (a > 0 && b > 0) {
+			return a + b;
+		} else {
+			throw new IllegalArgumentException("Both parameters must be positive!");
+		}
+	}
+	
+	/*
+	 * Most carnival rides have a minimum height requirement of 48 inches. This
+	 * method verifies the minimum height of a rider if only inches are provided.
+	 */	
+	public boolean verifyMinimumHeightFourFeet(int inches) {
+		if( inches > 0 ) {
+			return inches >= 48;
+		}else {
+			throw new IllegalArgumentException("Parameter must be positive!");
+		}
+	}
+	
+	/*
+	 * Most carnival rides have a minimum height requirement of 48 inches. This
+	 * method verifies the minimum height of a rider if both feet and inches are provided.
+	 */	
+	public boolean verifyMinimumHeightFourFeet(int feet, int inches) {
+		int inchesTotal = feet * 12 + inches;
+		if( inchesTotal > 0 ) {
+			return inchesTotal >= 48;
+		}else {
+			throw new IllegalArgumentException("Both parameters must be positive!");
+		}
+	}
+}
---
+++ src/test/java/com/promineotech/TestDemoJUnitTest.java
@@ -0,0 +1,82 @@
+package com.promineotech;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class TestDemoJUnitTest {
+
+	private TestDemo testDemo;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		testDemo = new TestDemo();
+	}
+
+	/* 
+	 * Parameterized test method for addPositive method in TestDemo 
+	 */
+	@ParameterizedTest
+	@MethodSource("com.promineotech.TestDemoJUnitTest#argumentsForAddPositive")
+	void assertThatTwoPositiveNumbersAreAddedCorrectly(int a, int b, int expected, boolean expectException) {
+		if (!expectException) {
+			assertThat(testDemo.addPositive(a, b)).isEqualTo(expected);
+		} else {
+			assertThatThrownBy(() -> testDemo.addPositive(a, b)).isInstanceOf(IllegalArgumentException.class);
+		}
+	}
+	
+	/* 
+	 * Parameterized test method arguments 
+	 */
+	static Stream<Arguments> argumentsForAddPositive() {
+		//@formatter:off
+		return Stream.of(
+			arguments(123,321,444,false),
+			arguments(2,4,6,false),
+			arguments(0,4,0,true),
+			arguments(2,0,0,true),
+			arguments(0,0,0, true),
+			arguments(-1,6,0,true),
+			arguments(2,-1,0,true)
+		);
+		//@formatter:on
+	}
+	
+	/*
+	 * Regular JUnitTest for addPositive method in TestDemo
+	 */
+	@Test
+	void assertThatPairsOfPositiveNumbersAreAddedCorrectly() {
+		assertThat(testDemo.addPositive(4,5)).isEqualTo(9);
+		assertThat(testDemo.addPositive(1,5)).isEqualTo(6);
+		assertThat(testDemo.addPositive(40,100)).isEqualTo(140);
+		assertThat(testDemo.addPositive(40,50)).isEqualTo(90);
+	}
+	
+	/*
+	 * Regular JUnitTest for verifyMinimumHeightFourFeet methods in TestDemo
+	 */
+	@Test
+	void assertThatRiderIsTallEnough() {
+		assertThat(testDemo.verifyMinimumHeightFourFeet(47)).isEqualTo(false);
+		assertThat(testDemo.verifyMinimumHeightFourFeet(48)).isEqualTo(true);
+		assertThat(testDemo.verifyMinimumHeightFourFeet(50)).isEqualTo(true);
+		assertThat(testDemo.verifyMinimumHeightFourFeet(0,48)).isEqualTo(true);
+		assertThat(testDemo.verifyMinimumHeightFourFeet(5, 0)).isEqualTo(true);
+		assertThatThrownBy(() -> testDemo.verifyMinimumHeightFourFeet(0)).isInstanceOf(IllegalArgumentException.class);
+		assertThatThrownBy(() -> testDemo.verifyMinimumHeightFourFeet(-1)).isInstanceOf(IllegalArgumentException.class);
+		assertThatThrownBy(() -> testDemo.verifyMinimumHeightFourFeet(0, 0)).isInstanceOf(IllegalArgumentException.class);
+		assertThatThrownBy(() -> testDemo.verifyMinimumHeightFourFeet(-7, 0)).isInstanceOf(IllegalArgumentException.class);
+		assertThatThrownBy(() -> testDemo.verifyMinimumHeightFourFeet(1, -12)).isInstanceOf(IllegalArgumentException.class);
+	}
+
+}
