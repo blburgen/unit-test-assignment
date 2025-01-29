d3781c991ab783616b2a284a7048d6b9af122d0f
Brady Burgener <bradylbsee@gmail.com>
2025-01-29 12:47:16 -0700

    adds randomNumberSquared method

--- src/main/java/com/promineotech/TestDemo.java
+++ src/main/java/com/promineotech/TestDemo.java
@@ -1,5 +1,7 @@
 package com.promineotech;
 
+import java.util.Random;
+
 public class TestDemo {
 
 	/*
@@ -13,29 +15,46 @@ public class TestDemo {
 			throw new IllegalArgumentException("Both parameters must be positive!");
 		}
 	}
-	
+
 	/*
 	 * Most carnival rides have a minimum height requirement of 48 inches. This
 	 * method verifies the minimum height of a rider if only inches are provided.
-	 */	
+	 */
 	public boolean verifyMinimumHeightFourFeet(int inches) {
-		if( inches > 0 ) {
+		if (inches > 0) {
 			return inches >= 48;
-		}else {
+		} else {
 			throw new IllegalArgumentException("Parameter must be positive!");
 		}
 	}
-	
+
 	/*
 	 * Most carnival rides have a minimum height requirement of 48 inches. This
-	 * method verifies the minimum height of a rider if both feet and inches are provided.
-	 */	
+	 * method verifies the minimum height of a rider if both feet and inches are
+	 * provided.
+	 */
 	public boolean verifyMinimumHeightFourFeet(int feet, int inches) {
 		int inchesTotal = feet * 12 + inches;
-		if( inchesTotal > 0 ) {
+		if (inchesTotal > 0) {
 			return inchesTotal >= 48;
-		}else {
+		} else {
 			throw new IllegalArgumentException("Both parameters must be positive!");
 		}
+	}
+
+	/*
+	 * returns a random integer multiplied by itself
+	 */
+	public int randomNumberSquared() {
+		int randomInt = getRandomInt();
+		return randomInt * randomInt;
+	}
+	
+	/*
+	 * returns a random integer
+	 */
+	int getRandomInt() {
+		Random random = new Random();
+		return random.nextInt(10) + 1;
 	}
 }
